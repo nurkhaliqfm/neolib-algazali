8f1ab071c37128fa02773c717c3bdef4529ee416
hadinitesolution <hadinsolution@gmailcom>
2025-06-01 01:45:30 +0800

    feat: standardize quotation marks and clean up SidebarUser component

--- src/components/layout/SidebarUser.tsx
+++ src/components/layout/SidebarUser.tsx
@@ -1,32 +1,31 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
-	DropdownMenuGroup,
 	DropdownMenuItem,
 	DropdownMenuLabel,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+} from "@/components/ui/dropdown-menu";
 import {
 	SidebarMenu,
 	SidebarMenuButton,
 	SidebarMenuItem,
 	useSidebar,
-} from '@/components/ui/sidebar';
+} from "@/components/ui/sidebar";
 
-import ProfilePic from '@/assets/avatar.png';
+import ProfilePic from "@/assets/avatar.png";
 import {
 	HiOutlineHome,
 	HiOutlineArrowRightOnRectangle,
 	HiOutlineChevronUpDown,
-} from 'react-icons/hi2';
-import { logout } from '@/modules/auth/services/oauthService';
-import { useDispatch } from 'react-redux';
-import { clearOAuthData } from '@/modules/auth/oauthSlice';
-import { useNavigate } from 'react-router-dom';
-import AppRoutes from '@/router/routes';
-import { toast } from 'react-toastify';
+} from "react-icons/hi2";
+import { logout } from "@/modules/auth/services/oauthService";
+import { useDispatch } from "react-redux";
+import { clearOAuthData } from "@/modules/auth/oauthSlice";
+import { useNavigate } from "react-router-dom";
+import AppRoutes from "@/router/routes";
+import { toast } from "react-toastify";
 
 export function SidebarUser({
 	user,
@@ -48,18 +47,18 @@ export function SidebarUser({
 				.then((status) => {
 					if (status) {
 						dispatch(clearOAuthData());
-						localStorage.removeItem('oauthData');
+						localStorage.removeItem("oauthData");
 					}
 				})
 				.then(() => {
 					navigate(AppRoutes.Login.path);
-					toast.success('Berhasil Logout', {
+					toast.success("Berhasil Logout", {
 						autoClose: 700,
 					});
 				})
 				.catch(() => {
-					toast.error('Gagal Logout', {
-						theme: 'colored',
+					toast.error("Gagal Logout", {
+						theme: "colored",
 						autoClose: 700,
 					});
 				});
@@ -77,7 +76,7 @@ export function SidebarUser({
 							<Avatar className="h-8 w-8 rounded-lg">
 								<AvatarImage
 									src={user.avatar ?? ProfilePic}
-									alt={user.name || ''}
+									alt={user.name || ""}
 								/>
 								<AvatarFallback className="rounded-lg">AD</AvatarFallback>
 							</Avatar>
@@ -92,7 +91,7 @@ export function SidebarUser({
 					</DropdownMenuTrigger>
 					<DropdownMenuContent
 						className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-						side={isMobile ? 'bottom' : 'right'}
+						side={isMobile ? "bottom" : "right"}
 						align="end"
 						sideOffset={4}>
 						<DropdownMenuLabel className="p-0 font-normal">
@@ -100,7 +99,7 @@ export function SidebarUser({
 								<Avatar className="h-8 w-8 rounded-lg">
 									<AvatarImage
 										src={user.avatar ?? ProfilePic}
-										alt={user.name ?? ''}
+										alt={user.name ?? ""}
 									/>
 									<AvatarFallback className="rounded-lg">AD</AvatarFallback>
 								</Avatar>
@@ -113,12 +112,10 @@ export function SidebarUser({
 							</div>
 						</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem>
-								<HiOutlineHome />
-								Home
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
+						<DropdownMenuItem onClick={() => navigate(AppRoutes.Home.path)}>
+							<HiOutlineHome />
+							Home
+						</DropdownMenuItem>
 						<DropdownMenuSeparator />
 						<DropdownMenuItem onClick={() => onLogout()}>
 							<HiOutlineArrowRightOnRectangle />
