4cfd27a728d7188ab60594313f6cb4875c1c192a
hadinitesolution <hadinsolution@gmailcom>
2025-06-07 11:18:11 +0800

    refactor: remove unused state and simplify SwiperButton component logic

--- src/modules/public/components/SwiperButton.tsx
+++ src/modules/public/components/SwiperButton.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@heroui/react";
-import { useState } from "react";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import SwiperInstance from "swiper";
 
@@ -8,15 +7,11 @@ interface SwiperButtonProps {
 }
 
 const SwiperControllButton = ({ swiperRef }: SwiperButtonProps) => {
-	const [activeIndexPagination, setActiveIndexPagination] = useState<number>(0);
-
 	const handleSwiperOnClick = ({ type }: { type: string }) => {
 		if (swiperRef.current) {
 			if (type === "next") {
-				setActiveIndexPagination(swiperRef.current.activeIndex + 1);
 				swiperRef.current.slideNext();
 			} else if (type === "prev") {
-				setActiveIndexPagination(swiperRef.current.activeIndex - 1);
 				swiperRef.current.slidePrev();
 			}
 		}
@@ -29,7 +24,6 @@ const SwiperControllButton = ({ swiperRef }: SwiperButtonProps) => {
 				radius="full"
 				variant="bordered"
 				color="primary"
-				isDisabled={activeIndexPagination === 0}
 				onPress={() => handleSwiperOnClick({ type: "prev" })}>
 				<HiChevronLeft />
 			</Button>
@@ -39,10 +33,6 @@ const SwiperControllButton = ({ swiperRef }: SwiperButtonProps) => {
 				radius="full"
 				size="sm"
 				variant="bordered"
-				isDisabled={
-					activeIndexPagination ===
-					(swiperRef.current?.pagination.bullets?.length ?? 0) - 2
-				}
 				color="primary"
 				onPress={() => handleSwiperOnClick({ type: "next" })}>
 				<HiChevronRight />
