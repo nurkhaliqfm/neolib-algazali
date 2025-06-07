8ca7ecf105498f7eef3b00dd29bf57506aa62621
hadinitesolution <hadinsolution@gmailcom>
2025-06-07 11:32:49 +0800

    fix: standardize string quotes in sidebar constants

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,4 +1,4 @@
-import AppRoutes from '@/router/routes';
+import AppRoutes from "@/router/routes";
 import {
 	HiOutlineAcademicCap,
 	HiOutlineBookOpen,
@@ -7,73 +7,90 @@ import {
 	HiOutlineChartPie,
 	HiOutlineUserGroup,
 	HiOutlineWallet,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 export const SIDEBAR_LIST = [
 	{
 		tag: null,
 		item: [
 			{
-				key: 'dashboard',
-				title: 'Dashboard',
+				key: "dashboard",
+				title: "Dashboard",
 				url: AppRoutes.AdminDashboard.path,
 				icon: HiOutlineChartPie,
+			},
+			{
+				key: "transaksi",
+				title: "Transaksi",
+				url: AppRoutes.AdminTransaksi.path,
+				icon: HiOutlineWallet,
 			},
 		],
 	},
 	{
-		tag: 'Koleksi',
+		tag: "Koleksi",
 		item: [
 			{
-				key: 'jurnal',
-				title: 'Jurnal',
+				key: "jurnal",
+				title: "Jurnal",
 				url: AppRoutes.AdminKoleksiJurnal.path,
 				icon: HiOutlineDocumentText,
 			},
 			{
-				key: 'ejurnal',
-				title: 'E-Journal',
+				key: "ejurnal",
+				title: "E-Journal",
 				url: AppRoutes.AdminKoleksiEjurnal.path,
 				icon: HiOutlineDocumentText,
 			},
 			{
-				key: 'buku',
-				title: 'Buku',
+				key: "buku",
+				title: "Buku",
 				url: AppRoutes.AdminKoleksiBuku.path,
 				icon: HiOutlineBookOpen,
 			},
 			{
-				key: 'ebook',
-				title: 'E-Book',
+				key: "ebook",
+				title: "E-Book",
 				url: AppRoutes.AdminKoleksiEbook.path,
 				icon: HiOutlineBookOpen,
 			},
 			{
-				key: 'skripsi',
-				title: 'Skripsi',
+				key: "skripsi",
+				title: "Skripsi",
 				url: AppRoutes.AdminKoleksiSkripsi.path,
 				icon: HiOutlineAcademicCap,
 			},
 		],
 	},
 	{
-		tag: 'Pengguna',
+		tag: "Pengguna",
 		item: [
 			{
-				key: 'transaksi',
-				title: 'Transaksi',
-				url: AppRoutes.AdminTransaksi.path,
-				icon: HiOutlineWallet,
-			},
-			{
-				key: 'anggota',
-				title: 'Data Anggota',
+				key: "mahasiswa",
+				title: "Mahasiswa",
 				url: AppRoutes.AdminAnggota.path,
 				icon: HiOutlineUserGroup,
 			},
 			{
-				key: 'password',
-				title: 'Ubah Password',
+				key: "dosen",
+				title: "Dosen",
+				url: AppRoutes.AdminAnggota.path,
+				icon: HiOutlineUserGroup,
+			},
+			{
+				key: "umum",
+				title: "Umum",
+				url: AppRoutes.AdminAnggota.path,
+				icon: HiOutlineUserGroup,
+			},
+		],
+	},
+	{
+		tag: "Akun",
+		item: [
+			{
+				key: "password",
+				title: "Ubah Password",
 				url: AppRoutes.AdminPassword.path,
 				icon: HiOutlineFingerPrint,
 			},
